e31bd9acac0273e361acb4a24edf2c18b1f05136
JoshiMitesh <miteshjoshi190@gmail.com>
2026-04-21 00:29:08 +0530

    Basics of Python

---
+++ day1.py
@@ -0,0 +1,34 @@
+# name = "mitesh"
+# age = 23
+
+# print("my name is", name)
+# print("my age is",age)
+
+# name = input("enter your name :  ")
+# age = int(input("enter your age:  "))
+# print("hello", name ,"your age is", age)
+
+# num1 = int(input("enter first num :"))
+# num2 = int(input("enter sec num :"))
+# print("your total is", num1 + num2)
+
+
+# a = int(input("enter value 1 :"))
+# b = int(input("enter value 2 :"))
+# c = a + b
+# print(c)
+
+
+# side = int(input("enter value of side : "))
+# print("area of square is ", side * side)
+
+# num1 = float(input("enter num1 : "))
+# num2 = float(input("enter num2 : "))  
+# print("average of num is : ", (num1 + num2)/2)
+
+
+a = int(input("enter a : ")) 
+b = int(input("enter b : "))
+
+print(a >= b)
+
